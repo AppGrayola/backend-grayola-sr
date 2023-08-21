dd57251a830df9c2fcdb437e2df19eaa8fb06b4f
Samuel Santa <s.santaa@uniandes.edu.co>
2023-08-21 12:02:47 -0500

    Cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,26 @@
 module.exports = [
   'strapi::errors',
   {
+
     name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
